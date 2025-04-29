fcf653d1d95a4eb71f837ac9348a9b1215d1ab48
Lucas Borges <lucasbborges2004@gmail.com>
2025-04-29 10:51:55 -0300

    Update aula 171
    Atualizei o script da aula 171 para fazer a soma das listas de acordo com a maior lista

--- Aulas_e_Exercicios/aula171-Resolucao_Exercicio_Somar_Listas.py
+++ Aulas_e_Exercicios/aula171-Resolucao_Exercicio_Somar_Listas.py
@@ -23,3 +23,11 @@ print(lista_soma)
 # for i, _ in enumerate(lista_b):
 #     lista_soma.append(lista_a[i] + lista_b[i])
 # print(lista_soma)
+
+# Em caso de querer fazer a soma de acordo com a maior lista:
+from itertools import zip_longest
+ 
+lista_a = [10, 2, 3, 4, 5]
+lista_b = [12, 2, 3, 6, 50, 60, 70]
+lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
+print(lista_soma)
